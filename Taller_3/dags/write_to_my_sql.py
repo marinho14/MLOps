d68e23902d1c1e2c85947be771860527067370ce
Sebastian Mariño <70497951+marinho14@users.noreply.github.com>
2024-03-08 13:02:58 -0500

    write penguins working

--- Taller_3/dags/write_to_my_sql.py
+++ Taller_3/dags/write_to_my_sql.py
@@ -15,31 +15,6 @@ def write_to_mysql():
     )
     cursor = conn.cursor()
 
-    # Crear la tabla si no existe
-    # create_table_query = """
-    #     CREATE TABLE IF NOT EXISTS penguins (
-    #         id INT AUTO_INCREMENT PRIMARY KEY,
-    #         studyName VARCHAR(255),
-    #         SampleNumber INT,
-    #         Species VARCHAR(255),
-    #         Region VARCHAR(255),
-    #         Island VARCHAR(255),
-    #         Stage VARCHAR(255),
-    #         IndividualID VARCHAR(255),
-    #         ClutchCompletion VARCHAR(255),
-    #         DateEgg DATE,
-    #         CulmenLength FLOAT,
-    #         CulmenDepth FLOAT,
-    #         FlipperLength FLOAT,
-    #         BodyMass INT,
-    #         Sex VARCHAR(255),
-    #         Delta15N FLOAT,
-    #         Delta13C FLOAT,
-    #         Comments VARCHAR(255)
-    #     )
-    # """
-    # cursor.execute(create_table_query)
-
     # create dataframe pandas
     df = pd.read_csv('/opt/airflow/data/penguins_lter.csv', sep=',')
     # Step 2: Create a SQLAlchemy engine to connect to the MySQL database
